de6c7c6b9ec4ec1aed765c2c80b4ddc2edb10531
vladyslavnUA <vladyslavn@yahoo.com>
2020-06-26 20:36:49 -0400

    receiving info

--- scrape.go
+++ scrape.go
@@ -6,25 +6,50 @@ import (
 	"github.com/gocolly/colly"
 )
 
+
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
+
+type bookItem struct {
+	Title string `json:"title"`
+	Rating string `json:"rating"`
+	Price string `json:"price"`
+	Image string `json:"image"`
+	Stock string `json:"stock"`
+}
+
+
+
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
-
+	books := []bookItem{}
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	c.OnHTML(".row ol > li", func(e *colly.HTMLElement) {
+
+		// link := e.Attr("href")
+		book := bookItem{}
+		book.Title = e.ChildText("h3")
+		book.Rating = e.ChildText(".star-rating")
+		book.Price = e.ChildText(".price_color")
+		book.Image = e.ChildText(".thumbnail")
+		book.Stock = e.ChildText(".availability")
+		books = append(books, book)
 
 		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Printf("Title: %q\n, Rating: %q\n, Price: %q\n, Image: %q\n, Stock: %q\n,", book.Title, book.Rating, book.Price, book.Image, book.Stock)
+                // fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 	})
+
+	// c.OnHTML("ol > li", func(e *colly.HTMLElement) {
+	// 	link := e.Attr("a")
+	// 	fmt.Printf("title: %t", e.Text, link)
+	// })
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("http://books.toscrape.com/")
 }
